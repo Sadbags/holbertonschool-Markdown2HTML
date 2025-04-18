6204a83a54a970dfdb1fb3fe06fa2b67ce742c2c
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-04-18 16:13:26 -0400

    adding task to run checks

---
+++ markdown2html.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+#checks for 2 arguments
+if len(sys.argv) < 3:
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+if not os.path.isfile(input_file):
+    sys.stderr.write(f"Missing {input_file}"):
+    sys.exit(1)
+
+sys.exit(0)
